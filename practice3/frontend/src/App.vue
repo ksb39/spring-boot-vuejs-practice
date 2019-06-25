66863374cc2b12a47a27def9acd5fa6193bcd61e
KwonSungBong <rnjstjdqhd3939@gmail.com>
2019-06-25 14:57:56 +0900

    init

---
+++ practice3/frontend/src/App.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  .App
+    AppHeader
+    AppNavigation
+    main.AppContent
+      router-view
+    AppFooter
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+  import AppHeader from '@/view/global/app-header'
+  import AppNavigation from '@/view/global/app-navigation'
+  import AppFooter from '@/view/global/app-footer'
+
+  export default {
+    name: 'App',
+    components: {
+      AppHeader,
+      AppNavigation,
+      AppFooter
+    },
+    computed: {
+      ...mapGetters('i18n', [
+        'langDefault'
+      ])
+    },
+    mounted () {
+      const payload = {
+        lang: this.langDefault,
+        app: this
+      }
+      this.$store.commit('i18n/LANG_CHANGE', payload, { root: true })
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import "stylesheet/app";
+</style>
---
+++ practice3/frontend/src/view/pages/Home/index.vue
@@ -0,0 +1,20 @@
+<i18n src="./i18n/index.json"></i18n>
+
+<template lang="pug">
+  .Home
+    h1.title {{ $t('title') }}
+
+</template>
+
+<script>
+  export default {
+    name: 'Home'
+  }
+</script>
+
+<style lang="scss">
+  .Home {
+    padding: 50px;
+    color: $color-primary;
+  }
+</style>
